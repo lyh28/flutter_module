4bbea0d7ff7d1f53ceca3aea8bb103b28a96b3ac
t_yihualuo <641740435@qq.com>
2019-08-27 15:59:07 +0800

    8-27

---
+++ .ios/Flutter/FlutterPluginRegistrant/Classes/GeneratedPluginRegistrant.m
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <flutter_boost/FlutterBoostPlugin.h>
+#import <flutter_ijkplayer/IjkplayerPlugin.h>
+#import <xservice_kit/XserviceKitPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterBoostPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBoostPlugin"]];
+  [IjkplayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"IjkplayerPlugin"]];
+  [XserviceKitPlugin registerWithRegistrar:[registry registrarForPlugin:@"XserviceKitPlugin"]];
+}
+
+@end
